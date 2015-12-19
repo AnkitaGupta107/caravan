bac59d14c6e0b14f38288253c5fe2ef124111186
khushbooivksp <khushboo.gupta@peppertap.com>
2015-12-20 02:50:03 +0530

    order detail page

--- app/src/main/java/com/peppertap/caravan/activities/CheckoutActivity.java
+++ app/src/main/java/com/peppertap/caravan/activities/CheckoutActivity.java
@@ -1,34 +1,25 @@
 package com.peppertap.caravan.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.peppertap.caravan.R;
 
 /**
  * Created by KhushbooGupta on 12/19/15.
  */
-public class CartActivity extends BaseActivity {
+public class CheckoutActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cart);
-
+        setContentView(R.layout.checkout_detail);
 
     }
 
     @Override
     protected void configureToolbar() {
 
-    }
-
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(this, CheckoutActivity.class);
-        startActivity(intent);
     }
 
     @Override
